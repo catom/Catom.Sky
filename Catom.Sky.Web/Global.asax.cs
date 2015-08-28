f6de2f9a9ac3eebb22486bca8ad325d3d4e9c3ef
jin.wang <jin.wang@greedyint.com>
2015-08-28 22:05:35 +0800

    修改了。

--- Catom.Sky.Web/Global.asax.cs
+++ Catom.Sky.Web/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Catom.Sky.Web.App_Start;
 
 namespace Catom.Sky.Web
 {
@@ -16,15 +17,26 @@ namespace Catom.Sky.Web
     {
         protected void Application_Start()
         {
+            #region 基本配置
             // 注册区域
             AreaRegistration.RegisterAllAreas();
 
             // 注册配置类
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            WebApiConfig.Register(GlobalConfiguration.Configuration);   // 注册 Web.config。
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);  // 过滤配置，如异常、鉴权过滤等（FilterAttribute, IAuthorizationFilter）。
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);  // 注册Web路由。
             BundleConfig.RegisterBundles(BundleTable.Bundles);  // 将css、js文件压缩打包成一个文件，提高请求效率。
             AuthConfig.RegisterAuth();  // 外站点账号鉴权注册。
+            #endregion 
+
+            #region 自定义配置
+            // IoC 注册
+            Bootstrapper.Instance.Initialise();
+            // 日志配置
+            log4net.Config.XmlConfigurator.Configure(new System.IO.FileInfo(Server.MapPath("~/log4net.config")));
+
+            #endregion
+
         }
 
         protected void Application_Shutdown()
