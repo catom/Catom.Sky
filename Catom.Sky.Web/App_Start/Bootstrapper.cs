da52f375778d572c1600b5ede1f792c1a2eb2f8e
jin.wang <jin.wang@greedyint.com>
2015-08-30 19:15:36 +0800

    添加了文件。

---
+++ Catom.Sky.Web/App_Start/Bootstrapper.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Specialized;
+using System.Configuration;
+using System.Web.Mvc;
+using Microsoft.Practices.Unity;
+using System.Data;
+using Microsoft.Practices.Unity.Mvc;
+
+namespace Catom.Sky.Web.App_Start
+{
+    public sealed class Bootstrapper
+    {
+        private static Bootstrapper strapper = null;
+
+        public static Bootstrapper Instance
+        {
+            get
+            {
+                if (strapper == null)
+                {
+                    strapper = new Bootstrapper();
+                }
+                return strapper;
+            }
+        }
+
+        private IUnityContainer container = null;
+
+        public IUnityContainer UnityContainer
+        {
+            get
+            {
+                return this.container;
+            }
+        }
+
+        public void Initialise()
+        {
+            BuildUnityContainer();
+            DependencyResolver.SetResolver(new UnityDependencyResolver(this.container));
+        }
+
+        public void BuildUnityContainer()
+        {
+            /*
+             * 
+             * Unity默认情况下会自动帮我们维护好这些对象的生命周
+             * 
+             * TransientLifetimeManager，瞬态生命周期，默认情况下，在使用RegisterType进行对象关系注册时如果没有指定生命周期管理器则默认使用这个生命周期管理器，这个生命周期管理器就如同其名字一样，当使用这种管理器的时候，每次通过Resolve或ResolveAll调用对象的时候都会重新创建一个新的对象。
+             * 
+             * ContainerControlledLifetimeManager，容器控制生命周期管理，这个生命周期管理器是RegisterInstance默认使用的生命周期管理器，也就是单件实例，UnityContainer会维护一个对象实例的强引用，每次调用的时候都会返回同一对象
+             * 
+            */
+
+            container = new UnityContainer();
+
+            //namevaluecollection nvc = configurationmanager.appsettings;
+            //string serverlist = nvc["serverlist"];
+            //string cachedarea = nvc["cachedarea"];
+            //string[] serverip = serverlist.split(',');
+            //imysession mysession = new mysession(serverip, int.parse(nvc["sessionexpirehours"]), nvc["sessioncookiedomain"], nvc["sessionarea"]);
+            //container.registerinstance(typeof(imysession), mysession);
+            //icache mycache = new memcache(serverip, cachedarea);
+            //icache mycache = new ocscache();
+            //container.registerinstance(typeof(icache), mycache);
+
+            #region 新的数据访问方式
+            //string readconn = configurationmanager.connectionstrings["schoolpalread"].connectionstring;
+            //string writeconn = configurationmanager.connectionstrings["schoolpalwrite"].connectionstring;
+            //container.registertype(typeof(iunitofwork), typeof(defaultunitofwork), "readunitofwork", new injectionconstructor(readconn));
+            //container.registertype(typeof(iunitofwork), typeof(defaultunitofwork), "writeunitofwork", new injectionconstructor(writeconn));
+            #endregion
+
+            #region 接口映射
+
+            #endregion
+        }
+    }
+}
+
---
+++ Catom.Sky.Web/App_Start/WebActivatorConfig.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using Microsoft.Practices.Unity.Mvc;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using System.Configuration;
+using Catom.Sky.Component;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(Catom.Sky.Web.App_Start.WebActivatorConfig), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethod(typeof(Catom.Sky.Web.App_Start.WebActivatorConfig), "Shutdown")]
+
+namespace Catom.Sky.Web.App_Start
+{
+    /// <summary>
+    ///  WebActivatorEx 作用等同 Global.asax.cs 启动配置。
+    /// </summary>
+    public static class WebActivatorConfig
+    {
+
+        /// <summary>
+        ///  Web 项目启动时。
+        /// </summary>
+        public static void Start()
+        {
+            #region 通过 UnityContainer 注册依赖关系
+
+            var container = UnityConfig.GetConfiguredContainer();
+            FilterProviders.Providers.Remove(FilterProviders.Providers.OfType<FilterAttributeFilterProvider>().First());
+            FilterProviders.Providers.Add(new UnityFilterAttributeFilterProvider(container));
+            DependencyResolver.SetResolver(container);
+            
+            #endregion
+
+
+        }
+
+        /// <summary>
+        ///  Web 项目停止时。
+        /// </summary>
+        public static void Shutdown()
+        {
+            // 注销 UnityContainer
+            var container = UnityConfig.GetConfiguredContainer();
+            container.Dispose();
+        }
+
+    }
+
+    #region 需要注册的配置类。
+    /// <summary>
+    /// IoC 容器注入。
+    /// </summary>
+    public class UnityConfig
+    {
+        // 延迟实例，构造 Unity 容器。
+        private static Lazy<IUnityContainer> container = new Lazy<IUnityContainer>(() =>
+        {
+            var container = new UnityContainer();
+            RegisterTypes(container);
+            return container;
+        });
+
+        public static IUnityContainer GetConfiguredContainer()
+        {
+            return container.Value;
+        }
+
+        /// <summary>
+        ///  在 UnityContainer 中注册 类型映射。
+        /// </summary>
+        public static void RegisterTypes(IUnityContainer container)
+        {
+            // NOTE: To load from web.config uncomment the line below. Make sure to add a Microsoft.Practices.Unity.Configuration to the using statements.
+            container.LoadConfiguration();
+
+            // TODO: Register your types here
+            #region 新的数据访问方式
+            // 读写分离（实例分离），注入 DAL 接口
+            string readConn = ConfigurationManager.ConnectionStrings["CatomSkyRead"].ConnectionString;
+            string writeConn = ConfigurationManager.ConnectionStrings["CatomSkyWrite"].ConnectionString;
+            //container.RegisterType(typeof(IUnitOfWork), typeof(DefaultUnitOfWork), "ReadUnitOfWork", new InjectionConstructor(readConn));
+            //container.RegisterType(typeof(IUnitOfWork), typeof(DefaultUnitOfWork), "WriteUnitOfWork", new InjectionConstructor(writeConn));
+            #endregion
+
+        }
+    }
+
+    #endregion
+
+}
