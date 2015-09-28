024959c08cce8578ca6effed61b67b6b24df933c
jin.wang <jin.wang@greedyint.com>
2015-09-28 18:12:33 +0800

    修改了 Content 里面的 css、js 路径； 新增了 BundleConfig.cs 文件，通过 bundle 来导入 css、js 文件。

--- Catom.Sky.Web/App_Start/BundleConfig.cs
+++ Catom.Sky.Web/App_Start/BundleConfig.cs
@@ -5,39 +5,68 @@ namespace Catom.Sky.Web
 {
     public class BundleConfig
     {
-    //    // 有关 Bundling 的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=254725
-    //    public static void RegisterBundles(BundleCollection bundles)
-    //    {
-    //        bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-    //                    "~/Scripts/jquery-{version}.js"));
+        // 有关 Bundling 的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkId=254725
+        // 将相对路径下的 CSS、JS 文件绑定到虚拟路径上去，在页面上通过 Styles.Render、Script.Render 调用虚拟路径。
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // Use Bundle rather than StyleBundle or ScriptBundle in order to turn off minification (takes the already minified files).
 
-    //        bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-    //                    "~/Scripts/jquery-ui-{version}.js"));
+            #region CSS Bundles
+            bundles.Add(new StyleBundle("~/Content/css/bootstrap")
+                .Include("~/Content/css/bootstrap/bootstrap.css")
+                .Include("~/Content/css/bootstrap/bootstrap-responsive.css")
+                .Include("~/Content/css/bootstrap/bootstrap-overrides.css"));
 
-    //        bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-    //                    "~/Scripts/jquery.unobtrusive*",
-    //                    "~/Scripts/jquery.validate*"));
+            bundles.Add(new Bundle("~/Content/css/font-awesome")
+                .Include("~/Content/css/font-awesome/font-awesome.css"));
 
-    //        // 使用要用于开发和学习的 Modernizr 的开发版本。然后，当你做好
-    //        // 生产准备时，请使用 http://modernizr.com 上的生成工具来仅选择所需的测试。
-    //        bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-    //                    "~/Scripts/modernizr-*"));
+            bundles.Add(new Bundle("~/Content/css/global")
+                .Include("~/Content/css/elements.css")
+                .Include("~/Content/css/icons.css")
+                .Include("~/Content/css/layout.css"));
 
-    //        bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new Bundle("~/Content/css/app")
+                .Include("~/Content/css/compiled/*.css")
+                .Include("~/Content/css/code-editor.css"));
 
-    //        bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-    //                    "~/Content/themes/base/jquery.ui.core.css",
-    //                    "~/Content/themes/base/jquery.ui.resizable.css",
-    //                    "~/Content/themes/base/jquery.ui.selectable.css",
-    //                    "~/Content/themes/base/jquery.ui.accordion.css",
-    //                    "~/Content/themes/base/jquery.ui.autocomplete.css",
-    //                    "~/Content/themes/base/jquery.ui.button.css",
-    //                    "~/Content/themes/base/jquery.ui.dialog.css",
-    //                    "~/Content/themes/base/jquery.ui.slider.css",
-    //                    "~/Content/themes/base/jquery.ui.tabs.css",
-    //                    "~/Content/themes/base/jquery.ui.datepicker.css",
-    //                    "~/Content/themes/base/jquery.ui.progressbar.css",
-    //                    "~/Content/themes/base/jquery.ui.theme.css"));
-    //    }
+            bundles.Add(new Bundle("~/Content/css/lib")
+                .Include("~/Content/css/lib/*.css"));
+
+            #endregion
+
+
+            #region Script Bundles
+            bundles.Add(new Bundle("~/Content/js/modernizr")
+                .Include("~/Content/js/modernizr/modernizr-2.6.2-respond-1.1.0.js"));
+
+            bundles.Add(new Bundle("~/Content/js/jquery")
+                .Include("~/Content/js/jquery/jquery-latest.js"));
+
+            bundles.Add(new Bundle("~/Content/js/jquery-ui")
+                .Include("~/Content/js/jquery/jquery-ui-1.10.2.custom.min.js"));
+
+            bundles.Add(new Bundle("~/Content/js/jquery.knob")
+                .Include("~/Content/js/jquery/jquery.knob.js"));
+
+            bundles.Add(new Bundle("~/Content/js/jquery.flot")
+                .Include("~/Content/js/jquery/jquery.flot.js")
+                .Include("~/Content/js/jquery/jquery.flot.resize.js")
+                .Include("~/Content/js/jquery/jquery.flot.stack.js"));
+
+            bundles.Add(new Bundle("~/Content/js/jquery.uniform")
+                .Include("~/Content/js/jquery/jquery.uniform.min.js"));
+
+            bundles.Add(new Bundle("~/Content/js/bootstrap")
+                .Include("~/Content/js/bootstrap/bootstrap.min.js")
+                .Include("~/Content/js/bootstrap/bootstrap.datepicker.js"));
+
+            bundles.Add(new Bundle("~/Content/js/theme")
+                .Include("~/Content/js/theme/theme.js"));
+
+            bundles.Add(new Bundle("~/Content/js/app")
+                .Include("~/Content/js/*.js"));
+
+            #endregion
+        }
     }
 }
