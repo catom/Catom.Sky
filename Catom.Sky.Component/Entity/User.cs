6411d7ad26fd46bf5c51b98fc6ed759c9485c515
catom <634358219@qq.com>
2015-10-06 16:43:07 +0800

    整理js包。

---
+++ Catom.Sky.Component/Entity/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
+
+namespace Catom.Sky.Component.Entity
+{
+    public class User
+    {
+        [PrimaryKey]
+        public long Id { set; get; }
+        public string Name { set; get; }
+        public string Pwd { set; get; }
+    }
+}
