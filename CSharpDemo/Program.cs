b9e4c7a161fb35c1a2f7403a088bd07d034c4b70
catom <634358219@qq.com>
2016-05-09 08:16:33 +0800

    获取代码

--- CSharpDemo/Program.cs
+++ CSharpDemo/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharpDemo
 {
     class Program
     {
+        enum PType
+        {
+            湖北 = 1,
+            浙江 = 2
+        }
+
         static void Main(string[] args)
         {
+            PType p = PType.湖北;
+            Console.WriteLine((int)PType.浙江);
         }
     }
 }
